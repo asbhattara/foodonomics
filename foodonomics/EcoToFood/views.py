53bf076c05e5fdeb2fac9f0924685ef6ac2dbdc9
sourabhlal <sourabhlal@yahoo.co.in>
2015-10-17 20:42:25 +0200

    Fixed yelp request
    Now can get multiple results

--- foodonomics/EcoToFood/views.py
+++ foodonomics/EcoToFood/views.py
@@ -15,7 +15,7 @@ def home(request):
 API_HOST = 'api.yelp.com'
 DEFAULT_TERM = 'dinner'
 DEFAULT_LOCATION = 'San Francisco, CA'
-SEARCH_LIMIT = 10
+SEARCH_LIMIT = 20
 SEARCH_PATH = '/v2/search/'
 BUSINESS_PATH = '/v2/business/'
 
@@ -28,6 +28,7 @@ TOKEN_SECRET = 'Vc3bYAxJN9HInpoEUOdi-gZJ4vA'
 def request(host, path, url_params=None):
     url_params = url_params or {}
     url = 'https://{0}{1}?'.format(host, urllib.quote(path.encode('utf8')))
+    print url
 
     consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
     oauth_request = oauth2.Request(method="GET", url=url, parameters=url_params)
@@ -86,26 +87,22 @@ def query_api(term, location):
     if not businesses:
         print u'No businesses for {0} in {1} found.'.format(term, location)
         return
+    responseList = []
+    for i in businesses:
+    	business_id = i['id']
+    	response = get_business(business_id)
+    	responseList.append(response)
 
-    business_id = businesses[0]['id']
-
-    print u'{0} businesses found, querying business info for the top result "{1}" ...'.format(
-        len(businesses),
-        business_id
-    )
-
-    response = get_business(business_id)
-
-    print u'Result for business "{0}" found:'.format(business_id)
-    return response
+    #print u'Result for business "{0}" found:'.format(business_id)
+    return responseList
 
 def main(request):
-    input_values = dict({'term': 'food', 'location': 'London'});
-
+    input_values = dict({'term': 'food', 'location': 'Vegesack'});
+    context = {}
     try:
-        context = query_api(input_values['term'], input_values['location'])
-        pprint.pprint(context, indent=2)
-        return render(request, 'base.html', context['location']['coordinate'])
+        context['listOfResponses'] = query_api(input_values['term'], input_values['location'])
+        #pprint.pprint(context, indent=2)
+        return render(request, 'base.html', context)
     except urllib2.HTTPError as error:
         sys.exit('Encountered HTTP error {0}. Abort program.'.format(error.code))
 
